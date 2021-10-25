c3ba6640201262f6a3c40e657d754e2246e84bab
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-10-25 23:56:45 +0600

    Estabilishing new routes and navigation through the pages

--- lib/BMICalculator/input_page.dart
+++ lib/BMICalculator/input_page.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'icon_content.dart';
 import 'constants.dart';
+import 'results_page.dart';
 
 enum Gender {
   male,
@@ -197,11 +198,18 @@ class _InputPageState extends State<InputPage> {
               ),
             ),
           ])),
-          Container(
-            color: kBottomContainerColor,
-            margin: const EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ResultsPage()));
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColor,
+              margin: const EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
