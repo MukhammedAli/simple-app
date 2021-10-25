486bc238acc51067ab51ca0c7487631fd7739482
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-10-25 20:56:30 +0600

    Additional UI for weight container

--- lib/BMICalculator/input_page.dart
+++ lib/BMICalculator/input_page.dart
@@ -19,6 +19,7 @@ class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
   int height = 180;
   int weight = 60;
+  int age = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -132,11 +133,24 @@ class _InputPageState extends State<InputPage> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        RoundIconButton(icon: FontAwesomeIcons.minus),
+                        RoundIconButton(
+                            icon: FontAwesomeIcons.minus,
+                            onPressed: () {
+                              setState(() {
+                                weight--;
+                              });
+                            }),
                         SizedBox(
                           width: 10.0,
                         ),
-                        RoundIconButton(icon: FontAwesomeIcons.plus),
+                        RoundIconButton(
+                          icon: FontAwesomeIcons.plus,
+                          onPressed: () {
+                            setState(() {
+                              weight++;
+                            });
+                          },
+                        ),
                       ],
                     ),
                   ],
@@ -146,6 +160,40 @@ class _InputPageState extends State<InputPage> {
             Expanded(
               child: ReusableCard(
                 color: kActiveCardColor,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'AGE',
+                      style: kLabelTextStyle,
+                    ),
+                    Text(age.toString(), style: kNumberTextStyle),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RoundIconButton(
+                          icon: FontAwesomeIcons.minus,
+                          onPressed: () {
+                            setState(() {
+                              age--;
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        RoundIconButton(
+                          icon: FontAwesomeIcons.plus,
+                          onPressed: () {
+                            setState(() {
+                              age++;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ])),
@@ -172,22 +220,27 @@ class _InputPageState extends State<InputPage> {
 // ),
 
 class RoundIconButton extends StatelessWidget {
-  RoundIconButton({required this.icon});
+  RoundIconButton({required this.icon, required this.onPressed});
 
   final IconData icon;
+  final Function onPressed;
+
+  void onPressedFunc() {
+    onPressed();
+  }
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
       child: Icon(icon),
-      elevation: 6.0,
-      constraints: BoxConstraints.tightFor(
+      elevation: 0.0,
+      constraints: const BoxConstraints.tightFor(
         width: 56.0,
         height: 56.0,
       ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-      onPressed: () {},
+      shape: const CircleBorder(),
+      fillColor: const Color(0xFF4C4F5E),
+      onPressed: onPressedFunc,
     );
   }
 }
