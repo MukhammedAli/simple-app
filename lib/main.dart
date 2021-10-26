d0aa1443ec3e43f08082aa7b1cf7655a16d4d196
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-10-26 13:17:51 +0600

    Resolving errors"

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:myapp2/pages/home.dart';
-import 'package:myapp2/pages/loading.dart';
-import 'package:myapp2/pages/choose_loc.dart';
-import 'package:myapp2/pages/loading_page.dart';
-import 'package:myapp2/BMICalculator/bmimainpage.dart';
 
-void main() => runApp(MaterialApp(
-      initialRoute: '/bmire',
-      routes: {
-        // '/': (context) => Loading(),
-        // '/home': (context) => LoadingPage(),
-        // '/location': (context) => ChooseLocation(),
-        '/bmire': (context) => BMICalculator(),
-      },
-    ));
