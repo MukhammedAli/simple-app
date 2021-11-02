6d06812b49cf2acd60ce3279052a2de7f859dffd
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-11-02 13:24:27 +0600

    Note checkboxes for tasks

--- lib/notifications/widgets/task_tile.dart
+++ lib/notifications/widgets/task_tile.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 class TaskTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      title: Text('This is a task'),
+      trailing: TaskCheckbox(),
     );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool? isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: (newValue) {
+          setState(() {
+            isChecked = newValue;
+          });
+        });
   }
 }
